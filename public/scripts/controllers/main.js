1df23c7b07e315715ebf2753ffcf9349455aa3ed
Gavin Berkowitz <gberkowitz@logmein.com>
2015-12-10 23:42:28 -0500

    Style view

--- public/scripts/controllers/main.js
+++ public/scripts/controllers/main.js
@@ -1,18 +1,25 @@
 'use strict';
 
 angular.module('hackathon').controller('Main', function ($scope, action, buttonConfig) {
+    var getByValue = function(arr, value) {
+      for (var i=0, iLen=arr.length; i<iLen; i++) {
+        if (arr[i]._id == value) return arr[i];
+      }
+    }
+
     action.getActions(function(actions){
         $scope.actionsList = actions;
+        buttonConfig.getButtonConfig(function (currentConfig){
+            $scope.newButtonOneAction = getByValue($scope.actionsList,currentConfig.buttonOneAction);
+            $scope.newButtonTwoAction = getByValue($scope.actionsList,currentConfig.buttonTwoAction);
+        }, function(err){
+            console.log(err);
+        });
     }, function(err){
         console.log(err)
     });
 
-    buttonConfig.getButtonConfig(function (buttonConfig){
-        $scope.buttonOneAction = $scope.newButtonOneAction = buttonConfig.buttonOneAction;
-        $scope.buttonTwoAction = $scope.newButtonTwoAction = buttonConfig.buttonTwoAction;
-    }, function(err){
-        console.log(err);
-    });
+
 
     $scope.saveButtonConfig = function(){
         buttonConfig.saveButtonConfig($scope.newButtonOneAction, $scope.newButtonTwoAction,
